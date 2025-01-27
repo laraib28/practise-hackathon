33d9667b601e1994f0b3cf9c012218231bfeea88
laraib28 <laraibadnan297@gmail.com>
2025-01-28 02:39:08 +0500

    update code

--- src/app/action/actions.ts
+++ src/app/action/actions.ts
@@ -1,11 +1,32 @@
-// import { Product } from "@/components/home/ourProducts";
+import { Product } from "../types/product"
 
-// export const addToCart = (product : Product) => {
-//    const cart : Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
+
+export const addToCart = (product : Product) => {
+   const cart : Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
    
-//    const existingProduct = cart.findIndex(item => item.id === product.id)
+   const existingProduct = cart.findIndex(item => item.id === product.id)
 
-//    if(existingProduct > -1){
-//       cart[existingProduct].inventory
-//    }
-// }
+   if(existingProduct > -1){
+      cart[existingProduct].inventory += 1
+   }
+   else{
+      cart.push({
+         ...product, inventory:1
+      })
+   }
+   localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+export const removeFromCart = (productId : string) => {
+let cart : Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
+
+cart = cart.filter(item => item.id !== Number(productId)) 
+localStorage.setItem('cart', JSON.stringify(cart))
+}
+export const undateCartQuantity = (productId : string, quantity : number) => {
+   const cart : Product[] = JSON.parse(localStorage.getItem('cart') || '[]')
+   const productIndex = cart.findIndex((item => item._id === productId)
+ if(productIndex > -1){
+   cart[productIndex].inventory =quantity
+ }
+}
--- src/app/types/product.ts
+++ src/app/types/product.ts
@@ -1,6 +1,7 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 export interface Product {
+    _id: string;
     image: string | StaticImport;
     name: string;              
     description: string;        
